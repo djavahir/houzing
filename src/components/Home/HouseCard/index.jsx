40adeca47e29380ffb1afc7b39e862ee39e8c086
Javohir Turgunpolatov <javohirturgunpolatov505@gmail.com>
2022-12-26 16:42:02 +0500

    eslint libriory remove

---
+++ src/components/Home/HouseCard/index.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  Content,
+  Details,
+  Divider,
+  Icons,
+  Img,
+  Like,
+  User,
+} from "./style";
+import noImg from "../../assets/imgs/noimg.png";
+import user from "../../assets/imgs/user.png";
+import Bed from "../../assets/icon/bed.svg";
+import Bath from "../../assets/icon/bath.svg";
+import Car from "../../assets/icon/car.svg";
+import Ruler from "../../assets/icon/ruler.svg";
+import Love from "../../assets/icon/love.svg";
+import Resize from "../../assets/icon/resize.svg";
+
+const HouseCard = ({ data = {} }) => {
+  const {
+    address,
+    houseDetails,
+    salePrice,
+    price,
+    city,
+    attachments,
+    country,
+    description,
+    category,
+  } = data;
+  return (
+    <Container>
+      <Button>Featured</Button>
+      <Button right>For sale</Button>
+      <Img src={(attachments && attachments[0]?.imgPath) || noImg} />
+      <Content>
+        <User src={(attachments && attachments[1]?.imgPath) || user} />
+        <div className="subTitle inline">
+          {city},{country},{description}
+        </div>
+        <div className="info">{address || "Quincy St, Brooklyn, NY, USA"} -{category?.name || 'category'} {houseDetails?.room || 0}-rooms 
+        </div>
+        <Details>
+          <Details.Item>
+            <Icons src={Bed} />
+            <div className="info">{houseDetails?.beds || 0} Beds</div>
+          </Details.Item>
+          <Details.Item>
+            <Icons src={Bath} />
+            <div className="info">{houseDetails?.bath || 0} Baths</div>
+          </Details.Item>
+          <Details.Item>
+            <Icons src={Car} />
+            <div className="info">{houseDetails?.garage || 0} Garage</div>
+          </Details.Item>
+          <Details.Item>
+            <Icons src={Ruler} />
+            <div className="info">{houseDetails?.area || 0} Sq Ft</div>
+          </Details.Item>
+        </Details>
+      </Content>
+      <Divider />
+      <Content footer>
+        <Details.Item footer>
+          <div className="info">
+            <del>${price || 0}/mo</del>
+          </div>
+          <div className="subTitle">${salePrice || 0}/mo</div>
+        </Details.Item>
+        <Details.Item icon>
+          <Like src={Resize} />
+          <Like src={Love} />
+        </Details.Item>
+      </Content>
+    </Container>
+  );
+};
+
+export default HouseCard;
---
+++ src/components/Home/HouseCard/style.js
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+
+
+const Container = styled.div`
+position: relative;
+width: 100%;
+max-width: 380px;
+min-width: 343px;
+height: 429px;
+filter: drop-shadow(0px 20px 38px rgba(0,0,0,0.06)) drop-shadow(0px 7px 46px rgba(0,0,0,0.06))
+drop-shadow(0px 8px 15px rgba(0,0,0,0.06));
+border: 1px solid #E6E9EC;
+border-radius: 3px;
+`;
+
+const Img = styled.img`
+width: 100%;
+max-height: 220px;
+min-height: 220px;
+`;
+
+const Content = styled.div`
+padding: 0 20px;
+padding: 14.8px 20px;
+display: ${({ footer }) => footer && 'flex'};
+justify-content: ${({ footer }) => footer && 'space-between'};
+
+`;
+
+const Details = styled.div`
+display: flex;
+justify-content: space-between;
+margin-top: 16px;
+`;
+
+Details.Item = styled.div`
+align-items: ${({ footer }) => !footer && 'center'};
+display: flex;
+gap: ${({ icon }) => icon && '20px'};
+flex-direction: ${({ icon }) => !icon && 'column'};
+`;
+
+const Icons = styled.img``;
+
+const Like = styled.img`
+:active{
+ transform: scale(0.9);
+}
+cursor: pointer;
+`;
+
+
+const Divider = styled.div`
+background: #e6e9ec;
+height: 1px;
+width: 100%;
+`;
+
+const Button = styled.button`
+position: absolute;
+:active{
+    transform:scale(0.9)
+} 
+background: ${({ right }) => right ? '#0D263B' : '#0061DF'};
+border-radius: 3px;
+border: none;
+display:flex;
+align-items:center; 
+justify-content:center;
+max-width: 71px;
+width:100%;
+height:100%;
+max-height: 23px;
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 10px;
+line-height: 13px;
+color: #FFFFFF;
+margin-top:20px;
+left: ${({ right }) => !right && "20px"};
+right: ${({ right }) => right && "20px"};
+cursor: pointer;
+`;
+ 
+const User=styled.img`
+width:38px;
+height:38px;
+border-radius:50%;
+border:3px solid white;
+position: absolute; 
+right:23px;
+bottom: 190px
+`;
+
+export { User,Container, Button, Img, Content, Details, Divider, Like, Icons }
